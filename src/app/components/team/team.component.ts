8ed5c6b6a1dc14f9e354bb6d13224a4d539d06c6
Jonathan DIki <jonathandiki@outlook.com>
2024-09-29 03:12:36 +0200

    add liked button

--- src/app/components/team/team.component.ts
+++ src/app/components/team/team.component.ts
@@ -1,30 +1,50 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'ns-team',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './team.component.html',
   styleUrl: './team.component.css'
+
 })
 export class TeamComponent {
 
   title!: string;
   description!: string;
   number !: number;
+  like !: number;
   imageUrl!: string;
+  likeButtonText!: string;
+  userHasLiked!: boolean;
 
   ngOnInit(): void
   {
     this.title = 'USA-Team';
     this.description = 'My best friend';
     this.number = 28;
+    this.like = 0;
     this.imageUrl = "assets/images/win.jpg";
+    this.likeButtonText = "Like"
+    this.userHasLiked = true;
   }
 
-  addNumber(): void {
-    this.number += 1;
-}
+  onLike(): void {
+
+    if (this.userHasLiked)
+    {
+      this.like++;
+      this.likeButtonText = "Liked";
+      this.userHasLiked = false;
+    } else
+    {
+      this.like--;
+      this.likeButtonText = "Like"
+      this.userHasLiked = true;
+      }
+  }
+
 
 
 }
